1b9e8e6648e1565ba343084c6cee832b8cea9078
François De Keersmaeker <francois.dekeersmaeker@uclouvain.be>
2025-08-21 10:32:24 +0000

    Expose function slugify_name

--- profile_translator_blocklist/__init__.py
+++ profile_translator_blocklist/__init__.py
@@ -2,11 +2,12 @@
 Package `profile-translator-blocklist`.
 """
 
-from .translator import translate_policy, translate_policies, translate_profile
+from .translator import slugify_name, translate_policy, translate_policies, translate_profile
 from .Policy import Policy
 
 
 __all__ = [
+    "slugify_name",
     "translate_policy",
     "translate_policies",
     "translate_profile",
